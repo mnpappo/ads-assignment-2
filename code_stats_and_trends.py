307f8a233068d4f09670821c1049cb5861cef38c
Nadimozzaman Pappo <mnpappo@gmail.com>
2023-12-15 15:43:52 +0000

    try heatmap, add both co2 and elec

--- code_stats_and_trends.py
+++ code_stats_and_trends.py
@@ -7,7 +7,7 @@ Used Python Version: 3.11.4
 Format: Black
 """
 
-
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -18,20 +18,27 @@ file_path = "./API_19_DS2_en_csv_v2_5998250/API_19_DS2_en_csv_v2_5998250.csv"
 
 # Read the CSV data
 df = pd.read_csv(file_path, skiprows=4)
-print(df.columns)
-print(df.head())
-print(df.describe())
+
+
+def explore_statistics(df):
+    """
+    Explore statistical properties
+    params: df
+    return: None
+    """
+    print(df.describe())
+
 
 # CO2 Indicators
-# indicators = [
-#     "EN.ATM.CO2E.SF.KT",
-#     "EN.ATM.CO2E.LF.KT",
-#     "EN.ATM.CO2E.KT",
-#     "EN.ATM.CO2E.GF.KT",
-# ]
+indicators_co2 = [
+    "EN.ATM.CO2E.SF.KT",
+    "EN.ATM.CO2E.LF.KT",
+    "EN.ATM.CO2E.KT",
+    "EN.ATM.CO2E.GF.KT",
+]
 
 # Electricity Indicators
-indicators = [
+indicators_elec = [
     "EG.ELC.RNEW.ZS",
     "EG.ELC.PETR.ZS",
     "EG.ELC.NUCL.ZS",
@@ -64,8 +71,6 @@ def process_data(indicators, df):
 
     # Transpose the data to have years as rows
     combined_data_transposed = combined_data[year_columns].transpose()
-    print(combined_data_transposed.head())
-    print(combined_data_transposed.describe())
 
     # Convert the index to datetime, now index represents years
     combined_data_transposed.index = pd.to_datetime(
@@ -77,9 +82,6 @@ def process_data(indicators, df):
         pd.to_numeric, errors="coerce"
     )
     return combined_data, combined_data_transposed
-
-
-combined_data, combined_data_transposed = process_data(indicators, df)
 
 
 def visualize_data(combined_data_transposed, indicators):
@@ -107,7 +109,6 @@ def visualize_data(combined_data_transposed, indicators):
             combined_data_transposed.index,
             combined_data_transposed[("USA", indicator)],
             label=f"USA {indicator}",
-            # label=combined_data.at[indicator, "Indicator Name"],
             marker="o",
         )
         # Plot China data
@@ -130,4 +131,58 @@ def visualize_data(combined_data_transposed, indicators):
     plt.show()
 
 
-visualize_data(combined_data_transposed, indicators)
+# visalize heat map
+# def visualize_heat_map(combined_data_transposed, indicators):
+#     """
+#     Visualize the data
+#     params: df
+#     return: None
+#     """
+#     # Normalize the data for heatmap
+#     normalized_data = (combined_data_transposed - combined_data_transposed.min()) / (
+#         combined_data_transposed.max() - combined_data_transposed.min()
+#     )
+
+#     for indicator in indicators:
+#         plt.figure(figsize=(10, 8))
+
+#         # Get the data for the heatmap
+#         heatmap_data = normalized_data.xs(
+#             indicator, level="Indicator Code", axis=1
+#         ).values
+
+#         # Create the heatmap
+#         plt.imshow(heatmap_data, aspect="auto", cmap="viridis")
+
+#         # Add color bar
+#         plt.colorbar()
+
+#         # Add titles and labels
+#         plt.title(f"Heatmap of {indicator}")
+#         plt.ylabel("Year")
+#         plt.xlabel("Country")
+
+#         # Set the y-axis labels to years
+#         plt.yticks(
+#             np.arange(len(combined_data_transposed.index)),
+#             [year.year for year in combined_data_transposed.index],
+#         )
+
+#         # Set the x-axis labels to countries
+#         plt.xticks(np.arange(2), combined_data_transposed.columns.levels[0])
+
+#         plt.show()
+
+
+explore_statistics(df)
+# CO2 data
+combined_data_co2, combined_data_transposed_co2 = process_data(indicators_co2, df)
+explore_statistics(combined_data_transposed_co2)
+visualize_data(combined_data_transposed_co2, indicators_co2)
+
+# Electricity data
+combined_data_elec, combined_data_transposed_elec = process_data(indicators_elec, df)
+explore_statistics(combined_data_transposed_elec)
+visualize_data(combined_data_transposed_elec, indicators_elec)
+
+# visualize_heat_map(combined_data_transposed, indicators)
